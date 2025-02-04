534bf67a9b168ffa7afd7e75dbc2040be29cf2e8
Zahira <zrivrio2103@gmail.com>
2025-02-04 11:47:56 +0100

    ya funciona el proyecto ventas

--- SEGUNDO_T/roto/src/main/java/org/iesbelen/excepcion/MiExcepcion.java
+++
@@ -1,8 +0,0 @@
-package org.iesbelen.excepcion;
-
-public class MiExcepcion  extends Exception{
-    private static final long serialVersionUID = 1L;
-    public MiExcepcion(String s){
-        super("{error.mi.error}");
-    }
-}
