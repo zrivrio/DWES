fe590786a3719000147eccaec9308fc6f4e77603
zrivrio <zrivrio2103@gmail.com>
2025-02-17 14:38:30 +0100

    elementcollections

---
+++ SEGUNDO_T/videoclub_jpa_rest-master/src/main/java/org/iesbelen/videoclub/domain/Address.java
@@ -0,0 +1,24 @@
+package org.iesbelen.videoclub.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+
+@Embeddable
+public class Address {
+
+    private int id;
+
+    private String street;
+
+    private int houseNumber;
+
+    private String city;
+
+    private int zipCode;
+}
--- SEGUNDO_T/videoclub_jpa_rest-master/src/main/java/org/iesbelen/videoclub/domain/Socio.java
+++ SEGUNDO_T/videoclub_jpa_rest-master/src/main/java/org/iesbelen/videoclub/domain/Socio.java
@@ -5,6 +5,9 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.NaturalId;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Entity
 @Table(name = "socios")
@@ -24,6 +27,21 @@ public class Socio{
     private String nombre;
 
     private String apellido;
+
+    @ElementCollection
+    @CollectionTable(name = "socio_addresses",joinColumns = @JoinColumn(name = "socio_id"))
+    @AttributeOverrides({
+            @AttributeOverride( name = "houseNumber", column = @Column(name = "house_number")),
+            @AttributeOverride( name = "street", column = @Column(name = "street")),
+            @AttributeOverride( name = "city", column = @Column(name = "city")),
+            @AttributeOverride( name = "zipCode", column = @Column(name = "zipCode"))
+    })
+    private Set<Address> addresses = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "persone_phone_numbers", joinColumns = @JoinColumn(name = "socio_id"))
+    @Column(name = "phone_number")
+    private Set<String> phoneNumbers = new HashSet<>();
 
     @OneToOne
     @JoinColumn( name = "tarjeta_id", referencedColumnName = "id")
