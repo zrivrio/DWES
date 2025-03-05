57cc41f15c5b9bc3ad66e5508617b0f26637549a
zrivrio <zrivrio2103@gmail.com>
2025-03-05 14:41:59 +0100

    preparacion examen

--- SEGUNDO_T/prueba/src/main/java/com/example/prueba/controller/PerfilController.java
+++ SEGUNDO_T/prueba/src/main/java/com/example/prueba/controller/PerfilController.java
@@ -1,4 +1,26 @@
 package com.example.prueba.controller;
 
+import com.example.prueba.domain.Perfil;
+import com.example.prueba.service.PerfilService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/perfil")
 public class PerfilController {
+
+    @Autowired
+    private PerfilService perfilService;
+
+    @GetMapping({"", "/"})
+    public List<Perfil> getPerfil() {
+        return perfilService.findAll();
+    }
+
+    @PostMapping({"", "/"})
+    public Perfil addPerfil(@RequestBody Perfil perfil) {
+        return perfilService.save(perfil);
+    }
 }
--- SEGUNDO_T/prueba/src/main/java/com/example/prueba/controller/ProductoController.java
+++ SEGUNDO_T/prueba/src/main/java/com/example/prueba/controller/ProductoController.java
@@ -1,4 +1,47 @@
 package com.example.prueba.controller;
 
+import com.example.prueba.domain.Producto;
+import com.example.prueba.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
 public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping({"", "/"})
+    public List<Producto> getAll() {
+        return productoService.findAll();
+    }
+
+    @PostMapping({"", "/"})
+    public Producto addProducto(@RequestBody Producto producto) {
+        return productoService.save(producto);
+    }
+
+    @GetMapping("/{id}")
+    public Producto getProducto(@PathVariable Long id) {
+        return productoService.one(id);
+    }
+
+    @PutMapping("/{id}")
+    public Producto updateProducto(@PathVariable Long id, @RequestBody Producto producto) {
+        return productoService.replace(id, producto);
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteProducto(@PathVariable Long id) {
+        productoService.delete(id);
+    }
+
+
+
 }
